ef1afdd6de8a72ec9133368ad1184d0475f596b4
pranzalkhadka <pranzalkhadka1@gmail.com>
2024-03-04 20:51:24 +0545

    Final changes

--- app/main.py
+++ app/main.py
@@ -3,11 +3,23 @@ import models
 from database import engine, get_db
 from routers import post, user, auth, vote
 
+from fastapi.middleware.cors import CORSMiddleware
+
 # Create the tables in the database
 models.Base.metadata.create_all(bind = engine)
 
 
 app = FastAPI()
+
+# Add the middleware to allow CORS
+# This is to allow requests from any domain on the internet
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
 
 
 # Include the routers in the main app        
